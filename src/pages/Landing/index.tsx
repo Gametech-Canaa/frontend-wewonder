b59e1d015230c62ca36125b72983da4abbfe1976
Caio de paula Brito <caiopbrito2005@gmail.com>
2021-03-06 18:00:23 -0400

    feature/crate groups and users

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -32,7 +32,7 @@ const Landing: React.FC = () => {
       <PageLandingContent id="landing-content" className="container">
         <LogoContainer>
           {/*<img src={logoImg} alt="COMPATIVIDADE" />*/}
-          <h1>ReplayWoman</h1>
+          <h1>HIGIA</h1>
           <h2>Slogan bonitinho da plataforma.</h2>
         </LogoContainer>
         <img
@@ -47,9 +47,9 @@ const Landing: React.FC = () => {
             Ver Grupos
           </Link>
 
-          <Link to="/give-classes" className="give-classes">
+          <Link to="/login" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas" />
-            Criar Grupos
+            Cadastre-se
           </Link>
         </ButtonsContainer>
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import TeacherList from "./pages/TeacherList";
 import TeacherForm from "./pages/TeacherForm";
+import LoginPage from "./pages/LoginPage"
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Landing} />
       <Route path="/study" component={TeacherList} />
-      <Route path="/give-classes" component={TeacherForm} />
+      <Route path="/give-classes/:id" component={TeacherForm} />
+      <Route path="/login" component={LoginPage}/>
     </BrowserRouter>
   );
 }
